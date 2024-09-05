6f2e7fe28bc17b91f57e80dea79180742ec657fa
Stanislava Gardasevic <stanislavabeograd@gmail.com>
2024-09-04 16:16:45 -1000

    Update profile.js
    name

--- data/profile.js
+++ data/profile.js
@@ -1,7 +1,7 @@
 const profileData = {
   title: "Resume",
-  name: "Vipin Yadav",
-  sub_title: "Senior Software Engineer",
+  name: "Stanislava (Stasha) Gardasevic",
+  sub_title: "Researcher and Metadata Specialist",
   logoURL: "assets/images/dp.jpg",
   about: {
     intro: `Senior Software Engineer with 6 years of frontend development experience. Expert in building high-performance UI and seamless UX. Experienced in leading projects, migrating legacy systems, and creating reusable components. Skilled in A/B testing, cross-platform compatibility, and responsive design. Effective mentor and collaborator dedicated to continuous learning and innovation.`,
